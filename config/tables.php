980c5d71b8dc478d232236597e08022f98c3f8b2
Slangenmans <68337699+Slangenmans@users.noreply.github.com>
2025-06-07 07:44:38 +0200

    tables migration and config

---
+++ config/tables.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'v9' => [
+        'steps' => 16,
+        'parameters' => [
+            "VOL",
+            "TSP",
+            "CMD_effect_1",
+            "CMD_value_1",
+            "CMD_effect_2",
+            "CMD_value_2"
+        ],
+    ],
+];
